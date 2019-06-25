4c7603f88b7650868aac72b6f7c31a3c9d7e65f3
jj2delgado <jj2delgado@yahoo.com>
2019-06-25 16:02:22 -0600

    solved black diamond

--- black-diamond/src/App.js
+++ black-diamond/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import axios from "axios";
-import StoreFront from "./Components/StoreFront/StoreFront";
-import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
-import NavBar from "./Components/NavBar/NavBar";
+import React, { Component } from "react"
+import axios from "axios"
+import StoreFront from "./Components/StoreFront/StoreFront"
+import ShoppingCart from "./Components/ShoppingCart/ShoppingCart"
+import NavBar from "./Components/NavBar/NavBar"
 
 class App extends Component {
   constructor(props) {
@@ -12,6 +12,9 @@ class App extends Component {
       cart: [],
       showCart: false
     };
+    this.addToCart = this.addToCart.bind(this)
+    this.navigate = this.navigate.bind(this)
+    this.removeFromCart = this.removeFromCart.bind(this)
   }
   componentDidMount() {
     axios
@@ -36,9 +39,9 @@ class App extends Component {
   }
   navigate(location) {
     if (location === "cart") {
-      this.state.showCart = true;
+      this.setState({showCart: true});
     } else {
-      this.state.showCart = false;
+      this.setState({showCart: false});
     }
   }
   render() {
