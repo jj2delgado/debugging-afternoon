0e3d38be5286279c79425e27c85600308762145e
jj2delgado <jj2delgado@yahoo.com>
2019-06-25 15:30:42 -0600

    solved

--- stage-2/src/Components/StoreFront/StoreFront.js
+++ stage-2/src/Components/StoreFront/StoreFront.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './StoreFront.css';
 
+
 class StoreFront extends Component {
   render() {
     let productDisplay = this.props.products.map((element, index) => {
@@ -16,6 +17,7 @@ class StoreFront extends Component {
     })
     return (
       <div className="storefront-container">
+        {productDisplay}
       </div>
     )
   }
